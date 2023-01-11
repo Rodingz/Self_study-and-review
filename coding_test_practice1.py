e5882218f4e31d4c4d4e427e344b67dc43f4f0c8
KangheeKim <113897847+Rodingz@users.noreply.github.com>
2023-01-11 22:39:24 +0800

    Add files via upload

---
+++ coding_test_practice1.py
@@ -0,0 +1,13 @@
+answer = 0
+t='3141592'
+p='271'
+part = []
+counter = 0
+while counter <= (len(t) - len(p)):
+    part.append(t[counter:counter + len(p)])
+    counter =+ 1
+for i in part: 
+    if i > int(p):
+        answer +=1
+
+print(answer)
